db18be3c165dd7ae0dc113a08a1f094d9cf26830
Filfeni <47361476+Filfeni@users.noreply.github.com>
2023-07-13 18:51:49 -0400

    initial project/database setup

---
+++ BankingTask.API/Data/Entities/Cuenta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankingTask.API.Data.Entities
+{
+    public partial class Cuenta
+    {
+        public Cuenta()
+        {
+            Movimientos = new HashSet<Movimiento>();
+        }
+
+        public int Id { get; set; }
+        public string NumeroCuenta { get; set; } = null!;
+        public int TipoCuenta { get; set; }
+        public int SaldoInicial { get; set; }
+        public bool Estado { get; set; }
+        public int ClienteId { get; set; }
+
+        public virtual Cliente Cliente { get; set; } = null!;
+        public virtual ICollection<Movimiento> Movimientos { get; set; }
+    }
+}
